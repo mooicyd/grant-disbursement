7ef3e1688579de681cf84970273b6f2b2f5b0aa1
mooicyd <mooicydexter@gmail.com>
2021-10-10 13:59:24 +0800

    Add getHouseholdById for grants controller

--- src/controllers/grants-controller.js
+++ src/controllers/grants-controller.js
@@ -1,3 +1,4 @@
 const householdService = require('../services/household-service')
 
 exports.addHousehold = async request => householdService.addHousehold(request.body)
+exports.getHouseholdById = async request => householdService.getHouseholdById(request.params.id)
--- src/controllers/grants-controller.spec.js
+++ src/controllers/grants-controller.spec.js
@@ -13,13 +13,33 @@ afterAll(async () => {
   await db.teardown()
 })
 
+const mockRequest = housingType => ({ body: { housingType } })
+
 describe('add household', () => {
-  const mockRequest = housingType => ({ body: { housingType } })
   it('valid', async () => {
     const housingType = 'HDB'
 
     const result = await grantsController.addHousehold(mockRequest(housingType))
 
     expect(result).toMatchObject({ id: result.id, familyMembers: [], housingType })
+  })
+})
+
+describe('get household by id', () => {
+  it('valid', async () => {
+    const housingType = 'HDB'
+
+    const newHousehold = await grantsController.addHousehold(mockRequest(housingType))
+    const household = await grantsController.getHouseholdById({ params: { id: newHousehold.id } })
+
+    expect(household).toBeTruthy()
+  })
+
+  it('household not found', async () => {
+    const household = await grantsController.getHouseholdById({
+      params: { id: '6162368212490dc38a9fe196' }
+    })
+
+    expect(household).toBeFalsy()
   })
 })
