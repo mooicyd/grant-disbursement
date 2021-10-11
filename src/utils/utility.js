fc31c5c7ebdddfdec7df3023a95b1d0220d4b855
mooicyd <mooicydexter@gmail.com>
2021-10-11 21:29:45 +0800

    Add utility function subtractYearsFromToday

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -1,1 +1,8 @@
 exports.isNumberString = value => value && !Number.isNaN(value)
+
+exports.subtractYearsFromToday = value => {
+  const date = new Date()
+  date.setFullYear(date.getFullYear() - value)
+  date.setUTCHours(0, 0, 0, 0)
+  return date
+}
