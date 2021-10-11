e9d78cc2d19b5210149561780c10495d763b42a1
mooicyd <mooicydexter@gmail.com>
2021-10-11 20:54:46 +0800

    Update mock family member data to ensure test consistency

--- src/test/mocks.js
+++ src/test/mocks.js
@@ -1,11 +1,15 @@
-const mockFamilyMemberData = () => ({
-  name: 'Dex',
-  gender: 'Male',
-  maritalStatus: 'Single',
-  spouse: '',
-  occupationType: 'Employed',
-  annualIncome: 10000,
-  dob: '1990-01-01'
-})
+const mockFamilyMemberData = () => {
+  const date = new Date()
+  date.setFullYear(date.getFullYear() - 20)
+  return {
+    name: 'Dex',
+    gender: 'Male',
+    maritalStatus: 'Single',
+    spouse: '',
+    occupationType: 'Employed',
+    annualIncome: 10000,
+    dob: date.toISOString().split('T')[0]
+  }
+}
 
 module.exports = { mockFamilyMemberData }
