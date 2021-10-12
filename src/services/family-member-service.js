28069ec48bc839a164a0d9922d27919620a5426e
mooicyd <mooicydexter@gmail.com>
2021-10-12 14:06:44 +0800

    Extract methods for query family aggregates

--- src/services/family-member-service.js
+++ src/services/family-member-service.js
@@ -3,6 +3,35 @@
 const FamilyMember = require('../models/family-member-model')
 const { subtractYearsFromToday, isNumberString } = require('../utils/utility')
 
+const hasCouple = householdId =>
+  FamilyMember.aggregate()
+    .match({ householdId })
+    .lookup({
+      from: 'familymembers',
+      localField: 'spouse',
+      foreignField: 'name',
+      as: 'spouseName'
+    })
+    .unwind('$spouseName')
+    .exec()
+
+const totalIncomeLessThanValue = (householdId, totalIncome) =>
+  FamilyMember.aggregate()
+    .match({ householdId })
+    .group({ _id: householdId, totalIncome: { $sum: '$annualIncome' } })
+    .match({ totalIncome: { $lt: Number(totalIncome) } })
+    .exec()
+
+const membersOlderThan = (householdId, age) =>
+  FamilyMember.aggregate()
+    .match({ householdId, dob: { $lt: subtractYearsFromToday(age) } })
+    .exec()
+
+const membersYoungerThan = (householdId, age) =>
+  FamilyMember.aggregate()
+    .match({ householdId, dob: { $gt: subtractYearsFromToday(age) } })
+    .exec()
+
 exports.addFamilyMember = async (familyMemberData, householdId) => {
   const newFamilyMember = new FamilyMember({ ...familyMemberData, householdId })
   return newFamilyMember.save()
@@ -14,44 +43,19 @@ exports.queryFamily = async (query, householdIds) => {
     const promises = []
 
     if (isNumberString(query.youngerThan)) {
-      promises.push(
-        FamilyMember.aggregate()
-          .match({ householdId, dob: { $gt: subtractYearsFromToday(query.youngerThan) } })
-          .exec()
-      )
+      promises.push(membersYoungerThan(householdId, query.youngerThan))
     }
 
     if (isNumberString(query.olderThan)) {
-      promises.push(
-        FamilyMember.aggregate()
-          .match({ householdId, dob: { $lt: subtractYearsFromToday(query.olderThan) } })
-          .exec()
-      )
+      promises.push(membersOlderThan(householdId, query.olderThan))
     }
 
     if (isNumberString(query.totalIncome)) {
-      promises.push(
-        FamilyMember.aggregate()
-          .match({ householdId })
-          .group({ _id: householdId, totalIncome: { $sum: '$annualIncome' } })
-          .match({ totalIncome: { $lt: Number(query.totalIncome) } })
-          .exec()
-      )
+      promises.push(totalIncomeLessThanValue(householdId, query.totalIncome))
     }
 
     if (query.hasCouple === 'true') {
-      promises.push(
-        FamilyMember.aggregate()
-          .match({ householdId })
-          .lookup({
-            from: 'familymembers',
-            localField: 'spouse',
-            foreignField: 'name',
-            as: 'spouseName'
-          })
-          .unwind('$spouseName')
-          .exec()
-      )
+      promises.push(hasCouple(householdId))
     }
 
     const results = await Promise.all(promises)
