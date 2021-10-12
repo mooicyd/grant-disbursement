a7e3471cd8616fde08ec170ab185a698a652ce63
mooicyd <mooicydexter@gmail.com>
2021-10-12 16:34:29 +0800

    Shift listening port to env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const server = require('./app')({ logger: true })
 
 const start = async () => {
   try {
-    await server.listen(3001)
+    await server.listen(process.env.APP_PORT)
   } catch (e) {
     server.log.error(e)
     process.exit(1)
